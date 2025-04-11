99fbae3e556043184d8a72c23e6bfdfc5d4e8a77
Gabriel Antonin Pascoali <gabrielantonin999@gmail.com>
2025-04-11 20:54:54 -0300

    feat: Implementação inicial

--- main.py
+++ main.py
@@ -1,7 +1,32 @@
 from fastapi import FastAPI
+from fastapi.middleware.cors import CORSMiddleware
+from pydantic import BaseModel
+import google.generativeai as genai
+import os
+from dotenv import load_dotenv
 
 app = FastAPI()
 
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=["*"],
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
+
+load_dotenv()
+genai.configure(api_key=os.getenv("GEMINI_API_KEY"))
+
+text_model = genai.GenerativeModel('gemini-1.5-flash')
+
+class PromptRequest(BaseModel):
+    prompt: str
+
 @app.get("/")
 async def root():
-    return {"message": "Hello World"}
+    return {"message": "Api iniciada."}
+
+@app.post("/ask")
+async def ask(request: PromptRequest):
+    response = text_model.generate_content(request.prompt)
+    return {"response": response.text}
